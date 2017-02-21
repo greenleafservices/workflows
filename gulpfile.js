06e41d1927cf1f8f480814239038cc065b154fd2
John Stegman <john@green-leaf-services.com>
2017-02-21 16:41:20 -0500

    Processed CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,15 +12,31 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ]; /* an array of sources */
+
+var sassSources = ['components/sass/style.scss'];
+
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
 });
+
 gulp.task('js', function() { /* the console command to run this task will be gulp js */
   gulp.src(jsSources) /* read the array of sources and place the js text into the script.js file */
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+  /* This next part tells compass how and where to build the css file */
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded' /* like normal css - so you can read it  Use compressed when you're ready to go live */
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
 });
